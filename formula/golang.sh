1b5f7fe332dce71496c6f7511326721e8a32d422
leleliu008 <leleliu008@gmail.com>
2022-07-06 18:38:08 +0800

    optimized

---
+++ formula/golang.sh
@@ -0,0 +1,3 @@
+UPPM_PKG_VER='1.18'
+UPPM_PKG_SHA='9cab6123af9ffade905525d79fc9ee76651e716c85f1f215872b5f2976782480'
+UPPM_PKG_URL='https://dl.google.com/go/go1.18.darwin-arm64.tar.gz'
---
+++ formula/jdk17.sh
@@ -0,0 +1,3 @@
+UPPM_PKG_VER='17.0.1'
+UPPM_PKG_SHA='8e5f240705bdf37ed831abc8c52cc05afe71721a6bc26dd977b6efd01b5ebef7'
+UPPM_PKG_URL='https://download.oracle.com/java/17/archive/jdk-17.0.1_macos-aarch64_bin.tar.gz'
